03670c90c8a4865bd25abd5bb4096d6be02c8055
jonathancaleb <jonathancaleb230@gmail.com>
2021-09-13 10:34:05 +0300

    project

---
+++ photos/urls.py
@@ -0,0 +1,10 @@
+from django.conf.urls import url
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.gallery, name='gallery'),
+    path('photo/<str:pk>/', views.viewPhoto, name='photo'),
+    path('add/', views.addphoto, name='add'),
+
+]
